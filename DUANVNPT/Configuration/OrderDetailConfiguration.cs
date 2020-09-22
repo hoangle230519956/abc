3793405091242bc2805dcd7d87b22eb71dbe95e0
Le Bao Hoang <hoanglb.hcm@vnpt.vn>
2020-09-22 18:27:38 +0700

    commit

---
+++ DUANVNPT/Configuration/OrderDetailConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using DUANVNPT.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DUANVNPT.Configuration
+{
+    public class OrderDetailConfiguration : IEntityTypeConfiguration<OrderDetail>
+    {
+        public void Configure(EntityTypeBuilder<OrderDetail> builder)
+        {
+            builder.ToTable("OrderDetails");
+            builder.HasOne(x => x.Order).WithMany(x => x.OrderDetails).HasForeignKey(x => x.OrderId);
+        }
+    }
+}
