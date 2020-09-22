0ae4ad20211742fab4da9ba56e43fdacc154d2b2
Le Bao Hoang <hoanglb.hcm@vnpt.vn>
2020-09-22 18:41:22 +0700

    master

--- DUANVNPT/Configuration/OrderDetailConfiguration.cs
+++ DUANVNPT/Configuration/OrderDetailConfiguration.cs
@@ -15,6 +15,7 @@ namespace DUANVNPT.Configuration
             builder.ToTable("OrderDetails");
             builder.HasKey(x => new { x.OrderId, x.ProductId });
             builder.HasOne(x => x.Order).WithMany(x => x.OrderDetails).HasForeignKey(x => x.OrderId);
+            builder.HasOne(x => x.Product).WithMany(x => x.OrderDetails).HasForeignKey(x => x.ProductId);
         }
     }
 }
