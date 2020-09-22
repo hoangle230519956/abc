2b6a8ad4e82e193dff2992d77c933b5d310b7ca9
Le Bao Hoang <hoanglb.hcm@vnpt.vn>
2020-09-22 18:17:03 +0700

    master

--- DUANVNPT/Configuration/OrderConfiguration.cs
+++ DUANVNPT/Configuration/OrderConfiguration.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using DUANVNPT.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DUANVNPT.Configuration
 {
-    public class OrderConfiguration
-    { 
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders");
+            builder.HasKey(x => x.Id);
+        }
     }
 }
