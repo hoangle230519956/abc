b89656e3783e994382847027ffde05bcf713b463
Le Bao Hoang <hoanglb.hcm@vnpt.vn>
2020-09-22 20:22:26 +0700

    master

--- DUANVNPT/Configuration/OrderConfiguration.cs
+++ DUANVNPT/Configuration/OrderConfiguration.cs
@@ -13,7 +13,7 @@ namespace DUANVNPT.Configuration
         public void Configure(EntityTypeBuilder<Order> builder)
         {
             builder.ToTable("Orders");
-            builder.HasKey(x => x.Id);
+            builder.HasKey(x => x.Id); 
             builder.Property(x => x.ShipEmail).IsRequired().IsUnicode(false).HasMaxLength(50);
         }
     }
