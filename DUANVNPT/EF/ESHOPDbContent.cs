9487d6e44f8702b575324f6dde8ecaa4b58af382
Le Bao Hoang <hoanglb.hcm@vnpt.vn>
2020-09-22 15:46:23 +0700

    Upload code

--- DUANVNPT/EF/ESHOPDbContent.cs
+++ DUANVNPT/EF/ESHOPDbContent.cs
@@ -1,4 +1,4 @@
-﻿using DUANVNPT.Entities;
+﻿//using DUANVNPT.Entities;
 using JetBrains.Annotations;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -13,28 +13,28 @@ namespace DUANVNPT.EF
         public ESHOPDbContent(DbContextOptions options) : base(options)
         {
         }
-        public DbSet<Products> Products { get; set; }
-        public DbSet<Category> Cotegories { get; set; }
-        public DbSet<AppConfig> AppConfigs { get; set; }
+        //public DbSet<Products> Products { get; set; }
+        //public DbSet<Category> Cotegories { get; set; }
+        //public DbSet<AppConfig> AppConfigs { get; set; }
 
 
-        public DbSet<Cart> Carts { get; set; }
+        //public DbSet<Cart> Carts { get; set; }
 
-        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
-        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+        //public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        //public DbSet<ProductInCategory> ProductInCategories { get; set; }
 
-        public DbSet<Contact> Contacts { get; set; }
+        //public DbSet<Contact> Contacts { get; set; }
 
-        public DbSet<Language> Languages { get; set; }
+        //public DbSet<Language> Languages { get; set; }
 
-        public DbSet<Order> Orders { get; set; }
+        //public DbSet<Order> Orders { get; set; }
 
-        public DbSet<OrderDetail> OrderDetails { get; set; }
-        public DbSet<ProductTranslation> ProductTranslations { get; set; }
+        //public DbSet<OrderDetail> OrderDetails { get; set; }
+        //public DbSet<ProductTranslation> ProductTranslations { get; set; }
 
-        public DbSet<Promotion> Promotions { get; set; }
+        //public DbSet<Promotion> Promotions { get; set; }
 
 
-        public DbSet<Transaction> Transactions { get; set; }
+        //public DbSet<Transaction> Transactions { get; set; }
     }
 }
