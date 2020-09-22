01ffab76b9c47c9732a90f7ba84a27ac2a3f7382
Le Bao Hoang <hoanglb.hcm@vnpt.vn>
2020-09-22 16:05:14 +0700

    acb

---
+++ DUANVNPT/Entities/Contact.cs
@@ -0,0 +1,18 @@
+﻿using DUANVNPT.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DUANVNPT.Entities
+{
+    public class Contact
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Email { set; get; }
+        public string PhoneNumber { set; get; }
+        public string Message { set; get; }
+        public Status Status { set; get; }
+    }
+}
---
+++ DUANVNPT/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DUANVNPT.Enums
+{
+    public enum OrderStatus
+    {
+        InProgress,
+        Confirmed,
+        Shipping,
+        Success,
+        Canceled
+    }
+}
