6b03da7834541dd342918f929a789301ef48491c
Le Bao Hoang <hoanglb.hcm@vnpt.vn>
2020-09-22 15:40:48 +0700

    abc

---
+++ DUANVNPT/EF/ESHOPDbContent.cs
@@ -0,0 +1,40 @@
+﻿using DUANVNPT.Entities;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DUANVNPT.EF
+{
+    public class ESHOPDbContent : DbContext
+    {
+        public ESHOPDbContent(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Category> Cotegories { get; set; }
+        public DbSet<AppConfig> AppConfigs { get; set; }
+
+
+        public DbSet<Cart> Carts { get; set; }
+
+        public DbSet<CategoryTranslation> CategoryTranslations { get; set; }
+        public DbSet<ProductInCategory> ProductInCategories { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Language> Languages { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductTranslation> ProductTranslations { get; set; }
+
+        public DbSet<Promotion> Promotions { get; set; }
+
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ DUANVNPT/Enums/TransactionStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DUANVNPT.Enums
+{
+    public enum TransactionStatus
+    {
+        Success,
+        Failed
+    }
+}
