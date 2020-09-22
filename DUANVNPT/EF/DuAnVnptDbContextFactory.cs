42acb34e939be44c81d632774e1474ca984ea16a
Le Bao Hoang <hoanglb.hcm@vnpt.vn>
2020-09-22 22:11:58 +0700

    master

---
+++ DUANVNPT/EF/DuAnVnptDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DUANVNPT.EF
+{
+    public class DuAnVnptDbContextFactory : IDesignTimeDbContextFactory<ESHOPDbContent>
+    {
+        public ESHOPDbContent CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsetting.json")
+               .Build();
+
+            var connectionString = configuration.GetConnectionString("eShopSolutionDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ESHOPDbContent>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ESHOPDbContent(optionsBuilder.Options);
+        }
+    }
+}
