1bd8c1fc11f1b915b45bce6d5b2c0a669b1e7be3
Le Bao Hoang <hoanglb.hcm@vnpt.vn>
2020-09-23 15:15:54 +0700

    maste

---
+++ DUANVNPT/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using DUANVNPT.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DUANVNPT.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppConfig>().HasData(new AppConfig()
+            {
+                Key = "HomeTile",
+                Value = "This is home page Du An VNPT"
+            });
+        }
+    }
+}
