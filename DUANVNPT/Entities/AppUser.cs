a3ae610ed901ae9d953cf085eed596b20dd250db
Le Bao Hoang <hoanglb.hcm@vnpt.vn>
2020-09-29 15:50:34 +0700

    master

--- DUANVNPT/Entities/AppUser.cs
+++ DUANVNPT/Entities/AppUser.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DUANVNPT.Entities
 {
-    public class AppUser
+    public class AppUser : IdentityUser<Guid>
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Dob { get; set; }
     }
 }
